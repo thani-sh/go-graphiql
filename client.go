32e69b70a64bf2a9b13f144f3352d63a995e9ac9
Muhammed Thanish <mnmtanish@gmail.com>
2016-05-24 17:12:20 +0530

    Add client code

---
+++ client.go
@@ -0,0 +1,95 @@
+package graphiql
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/url"
+)
+
+// GraphQLError ...
+type GraphQLError struct {
+	Message string `json:"message"`
+}
+
+// GraphQLError implements error interface
+func (e *GraphQLError) Error() string {
+	return e.Message
+}
+
+// Request ...
+type Request struct {
+	Query string `json:"query"`
+}
+
+// Response ...
+type Response struct {
+	Data   *json.RawMessage `json:"data"`
+	Errors []GraphQLError   `json:"errors,omitempty"`
+}
+
+// Client ...
+type Client struct {
+	Endpoint string
+	Header   http.Header
+	Client   http.Client
+}
+
+// NewClient ...
+func NewClient(uri string) (c *Client, err error) {
+	c = &Client{
+		Endpoint: uri,
+		Header:   http.Header{},
+		Client:   http.Client{},
+	}
+	c.Header.Set("Content-Type", "application/json")
+
+	u, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	if u.User == nil {
+		return c, nil
+	}
+
+	user := u.User.Username()
+	pass, hasPass := u.User.Password()
+	if user != "" && hasPass && pass != "" {
+		req := http.Request{}
+		req.SetBasicAuth(user, pass)
+		for k, v := range req.Header {
+			c.Header[k] = v
+		}
+	}
+
+	u.User = nil
+	c.Endpoint = u.String()
+
+	return c, nil
+}
+
+// Send ...
+func (c *Client) Send(req *Request) (res *Response, err error) {
+	body, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	bodyBuf := bytes.NewBuffer(body)
+	httpReq, err := http.NewRequest(http.MethodPost, c.Endpoint, bodyBuf)
+	if err != nil {
+		return nil, err
+	}
+	httpReq.Header = c.Header
+
+	httpRes, err := c.Client.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	res = &Response{}
+	if err := json.NewDecoder(httpRes.Body).Decode(res); err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
--- example/main.go
+++ example/main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/graphql-go/graphql"
 	"github.com/mnmtanish/go-graphiql"
 	"github.com/mnmtanish/go-graphiql/example/types"
 )
-
-// Request struct can be used to decode HTTP requests
-// sent by the GraphiQL IDE.
-type Request struct {
-	Query     string `json:"query"`
-	Variables string `json:"variables"`
-}
 
 func main() {
 	schema, err := graphql.NewSchema(graphql.SchemaConfig{
@@ -25,37 +19,52 @@ func main() {
 		panic(err)
 	}
 
+	go setMessage("Hello World")
+
 	http.HandleFunc("/", graphiql.ServeGraphiQL)
 	http.HandleFunc("/graphql", serveGraphQL(schema))
 	http.ListenAndServe(":9001", nil)
 }
 
+func setMessage(msg string) {
+	c, err := graphiql.NewClient("http://localhost:9001/graphql")
+	if err != nil {
+		panic(err)
+	}
+
+	q := `mutation _ { setMessage(msg: "` + msg + `") }`
+	res, err := c.Send(&graphiql.Request{Query: q})
+	if err != nil {
+		panic(err)
+	}
+
+	if string(*res.Data) != `{"setMessage":"Hello World"}` {
+		panic("bad response")
+	}
+
+	log.Println("listening on http://localhost:9001")
+}
+
 func serveGraphQL(s graphql.Schema) http.HandlerFunc {
-	return func(res http.ResponseWriter, req *http.Request) {
-		Err := func(errMsg string) {
-			res.WriteHeader(500)
-			res.Write([]byte(errMsg))
+	return func(w http.ResponseWriter, r *http.Request) {
+		sendError := func(err error) {
+			w.WriteHeader(500)
+			w.Write([]byte(err.Error()))
 		}
 
-		request := &Request{}
-		if err := json.NewDecoder(req.Body).Decode(request); err != nil {
-			Err("Internal Error")
+		req := &graphiql.Request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			sendError(err)
 			return
 		}
 
-		p := graphql.Params{Schema: s, RequestString: request.Query}
-		o := graphql.Do(p)
-		if o.HasErrors() {
-			Err("Internal Error")
-			return
-		}
+		res := graphql.Do(graphql.Params{
+			Schema:        s,
+			RequestString: req.Query,
+		})
 
-		result, err := json.Marshal(o)
-		if err != nil {
-			Err("Internal Error")
-			return
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			sendError(err)
 		}
-
-		res.Write(result)
 	}
 }
