cef5a61bd62b996aa67029c6c858f673d6e51718
Muhammed Thanish <mnmtanish@users.noreply.github.com>
2016-09-21 11:25:25 +0530

    Fix indents on server.go

--- server.go
+++ server.go
@@ -5,7 +5,6 @@ import "net/http"
 // Content ...
 var Content = []byte(`
 <!DOCTYPE html>
-
 <head>
   <style>body {height: 100vh; margin: 0; width: 100%; overflow: hidden;}</style>
   <link rel="stylesheet" href="//cdn.jsdelivr.net/sweetalert/1.1.3/sweetalert.css" />
@@ -16,41 +15,36 @@ var Content = []byte(`
   <script src="//cdn.jsdelivr.net/react/0.14.7/react-dom.min.js"></script>
   <script src="//cdn.jsdelivr.net/graphiql/0.6.3/graphiql.min.js"></script>
   <script>
-		(function () {
-			var PROMPT_OPTIONS = {
-				title: "GraphQL Endpoint",
-				text: "Please give the GraphQL HTTP Endpoint",
-				type: "input",
-				showCancelButton: false,
-				inputPlaceholder: window.location.origin + '/graphql',
-			};
-
-	    document.addEventListener('DOMContentLoaded', function () {
-				swal(PROMPT_OPTIONS, function(endpoint){
-					if (!endpoint) {
-						endpoint = window.location.origin + '/graphql';
-					}
-
-					function fetcher(params) {
-						var options = {
-							method: 'post',
-							headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
-							body: JSON.stringify(params),
-							credentials: 'include',
-						};
-
-						return fetch(endpoint, options)
-							.then(function (res) { return res.json() });
-					}
-
-					var body = React.createElement(GraphiQL, {fetcher: fetcher, query: '', variables: ''});
-					ReactDOM.render(body, document.body);
-				});
-	    });
-		}());
+    (function () {
+      var PROMPT_OPTIONS = {
+        title: "GraphQL Endpoint",
+        text: "Please give the GraphQL HTTP Endpoint",
+        type: "input",
+        showCancelButton: false,
+        inputPlaceholder: window.location.origin + '/graphql',
+      };
+      document.addEventListener('DOMContentLoaded', function () {
+        swal(PROMPT_OPTIONS, function(endpoint){
+          if (!endpoint) {
+            endpoint = window.location.origin + '/graphql';
+          }
+          function fetcher(params) {
+            var options = {
+              method: 'post',
+              headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+              body: JSON.stringify(params),
+              credentials: 'include',
+            };
+            return fetch(endpoint, options)
+              .then(function (res) { return res.json() });
+          }
+          var body = React.createElement(GraphiQL, {fetcher: fetcher, query: '', variables: ''});
+          ReactDOM.render(body, document.body);
+        });
+      });
+    }());
   </script>
 </head>
-
 <body>
 </body>
 `)
